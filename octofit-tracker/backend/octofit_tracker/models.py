4ba0a5e3d5c586add667d0b13dc4a8d1271ab804
Walter Truitt <wtruitt@hchb.com>
2025-07-10 13:42:00 +0000

    Populated database and setup api in django.

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,43 @@
+from djongo import models
+
+class User(models.Model):
+    _id = models.ObjectIdField()
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'users'
+
+class Team(models.Model):
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100)
+    members = models.ArrayReferenceField(to=User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'teams'
+
+class Activity(models.Model):
+    _id = models.ObjectIdField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity_type = models.CharField(max_length=100)
+    duration = models.DurationField()
+
+    class Meta:
+        db_table = 'activity'
+
+class Leaderboard(models.Model):
+    _id = models.ObjectIdField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    class Meta:
+        db_table = 'leaderboard'
+
+class Workout(models.Model):
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'workouts'
